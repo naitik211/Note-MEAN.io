316e998fbf90ac95ce4bba468a33dbfa146d5ecf
Naitik <naitik.211@gmail.com>
2014-08-11 16:28:43 +0530

    commit1

--- public/system/controllers/header.js
+++ public/system/controllers/header.js
@@ -1,7 +1,5 @@
 //developed by Naitik
 //Develop branch update
-//testing push revert
-//commit show
 'use strict';
 
 angular.module('mean.system').controller('HeaderController', ['$scope', '$rootScope', 'Global', 'Menus',
